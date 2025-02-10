19f1b7cb89d587dc1d13a81fe943553c69436feb
Axel Bergquist <axelthorwaldbergquist@gmail.com>
2025-02-10 18:04:36 +0100

    outline contt

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,25 @@
-import { useState } from 'react'
-import './App.css'
-import Leagues from './Leagues/Leagues'
-import Calendar from './Calendar/Calendar'
+  import { useState } from "react";
+  import "./App.css";
+  import Leagues from "./Leagues/Leagues";
+  import Calendar from "./Calendar/Calendar";
+  import Match from "./Match/Match";
 
-function App() {
-  return (
-    <div className='App'>
-      <Calendar></Calendar>
-      <Leagues></Leagues>
-      <Leagues></Leagues>
-      <Leagues></Leagues>
-      <Leagues></Leagues>
-      <Leagues></Leagues>
-    </div>
-  )
-}
+  function App() {
+    return (
+      <div className="App">
+        <Calendar />
+        <div className="content-container"> {/* Apply the class here */}
+          <div className="league-container">
+            <Leagues />
+            <Leagues />
+            <Leagues />
+            <Leagues />
+            <Leagues />
+          </div>
+          <Match />
+        </div>
+      </div>
+    );
+  }
 
-export default App
+  export default App;
---
+++ src/Match/Match.jsx
@@ -0,0 +1,11 @@
+import "./Match.css";
+
+const Match = () => {
+    return (
+        <div className="match">
+            <p>Match</p>
+        </div>
+    )
+}
+
+export default Match
