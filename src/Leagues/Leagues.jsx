9622214e6b1446426023ed6e80a7f5fc54d7f515
Axel Bergquist <axelthorwaldbergquist@gmail.com>
2025-02-10 17:40:40 +0100

    outline...

--- src/Leagues/Leagues.jsx
+++ src/Leagues/Leagues.jsx
@@ -1,6 +1,21 @@
 import "./Leagues.css";
 import ArrowRightIcon from '@mui/icons-material/ArrowRight';
-import { Link } from "@mui/icons-material";
+
+function fetchJSONData() {
+    fetch("https://www.thesportsdb.com/api/v1/json/3/searchteams.php?t=Arsenal")
+        .then((res) => {
+            if (!res.ok) {
+                throw new Error
+                    (`HTTP error! Status: ${res.status}`);
+            }
+            return res.json();
+        })
+        .then((data) => 
+              console.log(data))
+        .catch((error) => 
+               console.error("Unable to fetch data:", error));
+}
+fetchJSONData();
 
 const Leagues = () => {
   return (
