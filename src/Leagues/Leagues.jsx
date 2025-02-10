f73249a165da05eba5153b51f3430687658391e9
Axel Bergquist <axelthorwaldbergquist@gmail.com>
2025-02-10 16:52:37 +0100

    outline cont

--- src/Leagues/Leagues.jsx
+++ src/Leagues/Leagues.jsx
@@ -1,18 +1,29 @@
-import "./Leagues.css"
+import "./Leagues.css";
+import ArrowRightIcon from '@mui/icons-material/ArrowRight';
+import { Link } from "@mui/icons-material";
 
 const Leagues = () => {
-    return (
-        <div className="league">
-            <div className="league-name-box">
-            <h4 className="league-name">League name</h4>
-            <button className="league-redirect">League</button>
-            </div>
-            <div className="fixture-list">
-                <a href="" className="fixture"><div><div className="game-time">20:00</div>Team A vs Team B</div></a>
-                
-            </div>
-        </div>
-    )
-}
+  return (
+    <div className="league">
+      <div className="league-name-box">
+        <h4 className="league-name">League name</h4>
+        <a className="league-redirect" href=""><ArrowRightIcon></ArrowRightIcon></a>
+      </div>
+      <div className="fixture-list">
+        <a href="" className="fixture">
+          <div className="game-time">20:00</div>
+          <div className="score">
+            <div className="home-score">1</div>
+            <div className="away-score">2</div>
+          </div>
+          <div className="teams">
+            <div className="home-team">Home</div>
+            <div className="away-team">Away</div>
+          </div>
+        </a>
+      </div>
+    </div>
+  );
+};
 
-export default Leagues
+export default Leagues;
