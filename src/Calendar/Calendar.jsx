438dde021ddd391e5ee52f725e55f783893e2b2f
Axel Bergquist <axelthorwaldbergquist@gmail.com>
2025-02-10 17:34:06 +0100

    outline cont..

--- src/Calendar/Calendar.jsx
+++ src/Calendar/Calendar.jsx
@@ -1,9 +1,23 @@
-import "./Calendar.css"
+import "./Calendar.css";
+import CalendarMonthIcon from "@mui/icons-material/CalendarMonth";
+import ArrowLeftIcon from "@mui/icons-material/ArrowLeft";
+import ArrowRightIcon from "@mui/icons-material/ArrowRight";
 
 const Calendar = () => {
-    return (
-        <div className="Calendar">Calendar with dates</div>
-    )
-}
+  return (
+    <div className="Calendar">
+      <a href="" className="calendar-arrow">
+        <ArrowLeftIcon></ArrowLeftIcon>
+      </a>
+      <a href="" className="calendar-arrow">
+        <ArrowRightIcon></ArrowRightIcon>
+      </a>
+      <a href="" className="clickable-calendar">
+      <CalendarMonthIcon></CalendarMonthIcon>
+      <span>Today</span>
+      </a>
+    </div>
+  );
+};
 
-export default Calendar
+export default Calendar;
--- src/Leagues/Leagues.jsx
+++ src/Leagues/Leagues.jsx
@@ -5,9 +5,11 @@ import { Link } from "@mui/icons-material";
 const Leagues = () => {
   return (
     <div className="league">
-      <div className="league-name-box">
-        <h4 className="league-name">League name</h4>
-        <a className="league-redirect" href=""><ArrowRightIcon></ArrowRightIcon></a>
+      <div>
+        <a href="" className="league-name-box">
+            <h4 className="league-name">League name</h4>
+            <ArrowRightIcon></ArrowRightIcon>
+        </a>
       </div>
       <div className="fixture-list">
         <a href="" className="fixture">
