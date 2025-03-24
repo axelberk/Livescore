353b63b6b4a2c29d993b8ca3b95b877d3c4b9d35
Axel Bergquist <axelthorwaldbergquist@gmail.com>
2025-03-24 16:19:16 +0100

    date functionality

--- src/Calendar/Calendar.jsx
+++ src/Calendar/Calendar.jsx
@@ -6,30 +6,56 @@ import ArrowRightIcon from "@mui/icons-material/ArrowRight";
 
 const Calendar = () => {
   const [isOpen, setIsOpen] = useState(false)
+  const [selectedDate, setSelectedDate] = useState(new Date())
 
   const toggleModal = (e) => {
     e.preventDefault()
     setIsOpen(!isOpen)
   }
 
+  const formatDate = (date) => {
+    return date.toISOString().split("T")[0]
+  }
+
+  const handleDateChange = (e) => {
+    setSelectedDate(new Date(e.target.value))
+    setIsOpen(false)
+  }
+
+  const previousDay = () => {
+    setSelectedDate((prevDate) => {
+      const newDate = new Date(prevDate)
+      newDate.setDate(newDate.getDate() - 1)
+      return newDate
+    })
+  }
+
+  const nextDay = () => {
+    setSelectedDate((prevDate) => {
+      const newDate = new Date(prevDate)
+      newDate.setDate(newDate.getDate() + 1)
+      return newDate
+    })
+  }
+
   return (
     <div className="Calendar">
-      <a href="" className="calendar-arrow">
+      <a href="" className="calendar-arrow" onClick={previousDay}>
         <ArrowLeftIcon></ArrowLeftIcon>
       </a>
-      <a href="" className="calendar-arrow">
+      <a href="" className="calendar-arrow" onClick={nextDay}>
         <ArrowRightIcon></ArrowRightIcon>
       </a>
       <a href="" className="clickable-calendar" onClick={toggleModal}>
       <CalendarMonthIcon></CalendarMonthIcon>
-      <span>Today</span>
+      <span>{formatDate(selectedDate)}</span>
       </a>
 
       {isOpen && (
-        <div className="calendar-modal">
-          <div className="modal-content">
+        <div className="calendar-modal" onClick={toggleModal}>
+          <div className="modal-content" onClick={(e) => e.stopPropagation()}>
             <h3>Select a date</h3>
-            <input type="date" />
+            <input type="date" onChange={handleDateChange}/>
             <button onClick={toggleModal}>Close</button>
           </div>
         </div>
